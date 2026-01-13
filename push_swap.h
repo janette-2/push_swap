00ee0f78c2972c2e7244c4b22a5743311a56df82
Janette <janetterp2@gmail.com>
2026-01-13 21:17:38 +0100

    Header inicial

---
+++ push_swap.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 21:00:44 by janrodri          #+#    #+#             */
+/*   Updated: 2026/01/13 21:13:26 by janrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+//# include <limits.h>
+# include <stdio.h>
+
+typedef	struct s_stack
+{
+	int				value;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
+
+
+#endif
