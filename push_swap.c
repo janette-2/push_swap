aabddfb052d9bed212a734bb1dba6fbfdc4a436d
Janette <janetterp2@gmail.com>
2026-01-17 13:50:16 +0100

    Caso sin datos, ok

---
+++ push_swap.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/17 13:34:57 by janrodri          #+#    #+#             */
+/*   Updated: 2026/01/17 13:48:03 by janrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+		return (0);
+	printf("Entramos\n");
+}
+/* Como es un programa, cada vez que haces algún cambio, tienes
+que volver a compilar el archivo (para pruebas sin el Makefile) y
+después de hacerle cc [ddd.c] lo ejecutas con: ./a.out [datos] */
