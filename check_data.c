04f6cac8fb6781da39844498a7f6651ec544b73f
Janette <janetterp2@gmail.com>
2026-01-17 15:18:00 +0100

    Theory arguments in main

---
+++ check_data.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_data.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/17 14:15:23 by janrodri          #+#    #+#             */
+/*   Updated: 2026/01/17 15:17:15 by janrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/* is_single_number
+
+Description: Function to detect if the values given are isolated numbers in each 
+string or it is an string starting with "x y  z" that contains several numbers.
+To do so, it checks if there are arguments separatedby spaces in the string. 
+
+Arguments: The function analyses an *argv[x] (pointer) 
+extracted from the main().
+
+Returns: 1 or 0. 
+If the function returns 1, it means that the data found is a single number.
+If it is 0, it means that the value given is a block of numbers in a string.  
+
+*/
+
+int	is_single_number(char *argv)
+{
+	
+}
+
+// SHROTCUT COMMENTS: Truco para comentar una única linea (o selección de varias): presionar ctrl +k y ctrl + c 
+// Para descomentar sería ctrl + k y ctrl + u . 
+// Para bloques también puede usar shift + alt + a
+
+// FINAL *ARGV[]: Para identificar el final de los argumentos que ha recopilado main *argv[]
+// acaba siempre con NULL.
+
+// BASH QUOTING: Ten en cuenta que cuando se pasa una string por *argv[] cuando
+// vayas a leer el primer char de esa string, aunque se identifiquen con "" o ''
+// estos se interpretan como caracteres especiales de especificación de formato. 
+// NO VERÁS COMO char[0] = " PORQUE ES UNA CONVERSIÓN INTRÍNSECA A FORMATOS DE DATOS 
+// (ver en Quoting del man bash)
