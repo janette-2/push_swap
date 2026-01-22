500ba28a0195d26c1613c7d786eb55ef9222d44b
Janette Rodriguez Ponce <janerodpon@gmail.com>
2026-01-22 20:39:42 +0100

    Stack

---
+++ stack.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 19:42:10 by janrodri          #+#    #+#             */
+/*   Updated: 2026/01/22 20:37:28 by janrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/* stack_new
+
+Description: Function that initializes a new stack,
+stablishing the initial values of each attribute of
+a node (stack).
+
+Arguments: To create a stack you need at least the
+firts value that would keep a node from the stack.
+
+Returns: The first node that would create the stack.*/
+t_stack	*stack_new(int value)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->index = 0;
+	new->pos = -1;
+	new->target_pos = -1;
+	new->cost_a = -1;
+	new->cost_b = -1;
+	new->next = NULL;
+	return (new);
+}
