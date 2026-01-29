e40180941fc5199f4b32f205144abbf5cfb7d032
Janette <janetterp2@gmail.com>
2026-01-29 19:16:23 +0100

    Ajustadas las funcs de separación de datos en 1 archivo

--- separate_data.c
+++ separate_data.c
@@ -6,12 +6,44 @@
 /*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 18:33:12 by janrodri          #+#    #+#             */
-/*   Updated: 2026/01/23 20:47:14 by janrodri         ###   ########.fr       */
+/*   Updated: 2026/01/29 19:15:35 by janrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
+
+/* several_in_string
+
+Description: Function to detect if the values given are isolated numbers in each 
+string or it is an string starting with "x y  z" that contains several numbers.
+To do so, it checks if there are arguments separatedby spaces in the string. 
+
+Arguments: The function analyses an *argv[x] (pointer) 
+extracted from the main().
+
+Returns: 1 or 0. 
+If the function returns 1, it means that the data found is a single number.
+If it is 0, it means that the value given is a block of numbers in a string.  
+
+*/
+
+int	several_in_string(char *argv)
+{
+	if (ft_memchr(argv, ' ', ft_strlen(argv)))
+		return (1);
+	else
+		return (0);
+}
+
+/* separated_argv
+
+Description:
+
+Arguments:
+
+Returns:
+*/
 
 char	**separated_argv(char *argv)
 {
@@ -20,13 +52,15 @@ char	**separated_argv(char *argv)
 	separated_argv = ft_split(argv, ' ');
 	return (separated_argv);
 }
+
+
 /* checking_entry
 
 Description: Function that detects one by one, if each argument is valid.
 If the signs are valid, the range is inside the limits, and there are no 
 weird characters, it would be valid.
 
-Arguments: The single elements passed by arguments.
+Arguments: The single elements passed by arguments, already separated if needed.
 
 Returns: 1 or 0. It returns 1 if it is valid.
 It returns 0 if the argument is invalid.*/
@@ -38,6 +72,76 @@ int	check_entry(char *argv)
 	return (1);
 }
 
+
+/* nbr_elements
+
+Description:
+
+Arguments:
+
+Returns:*/
+
+
+int	nbr_elements(char *argv[])
+{
+	int		count;
+	int		i;
+	int		j;
+	char	**to_separate;
+
+	count = 0;
+	i = 1;
+	while (argv[i])
+	{
+		if (several_in_string(argv[i]))
+		{
+			to_separate = separated_argv(argv[i]);
+			j = 0;
+			while (to_separate[j++])
+				count++;
+		}
+		else
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+/* new_argv
+
+Description:
+
+Arguments:
+
+Returns:*/
+
+char	**new_argv(char *argv[])
+{
+	char	**new;
+	char	**to_separate;
+	int		i;
+	int		j;
+	int		k;
+
+	new = (char **)malloc((nbr_elements(argv) + 1) * sizeof(char *));
+	i = 1;
+	k = 0;
+	while (argv[i])
+	{
+		if (several_in_string(argv[i]))
+		{
+			to_separate = separated_argv(argv[i]);
+			j = 0;
+			while (to_separate[j])
+				new[k++] = to_separate[j++];
+			i++;
+		}
+		else
+			new[k++] = argv[i++];
+	}
+	new[k] = NULL;
+	return (new);
+}
 
 
 /* PROCEDIMIENTO PARA DETECTAR DATOS ERRÓNEOS:
