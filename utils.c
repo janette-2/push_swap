c379d442ab5d7604ef30c841cf3c03e0e3a40d70
Janette <janetterp2@gmail.com>
2026-02-04 20:58:34 +0100

    Fixed the leaks of clean_error

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 13:40:30 by janrodri          #+#    #+#             */
-/*   Updated: 2026/02/03 23:19:10 by janrodri         ###   ########.fr       */
+/*   Updated: 2026/02/04 20:56:11 by janrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@
 
 Description: Function that cleans the stack lists created in the program,
 writes in terminal an error message anc closes all the standard streams opened.
+Cleans the double pointer(*args), the strings inside the array of 
+strings(*args[i]) and the pointer to the array of strings(args) 
+[which is char ***args]. The function clear_stack takes care of erasing
+the nodes inside each stack item until the last one. Erasing a single stack
+deletes the variables inside of it but not the variable of the node that
+the address of 'next' is pointing to.
 
-Arguments: The stacks that the program manipulates, stack_a and stack_b
+Arguments: The stacks that the program manipulates, stack_a and stack_b. 
 
 Returns: Nothing, just executes the cleaning, informs of the status
 and exits the program.
@@ -26,12 +32,20 @@ and exits the program.
 
 void	clean_error(t_stack **stack_a, t_stack **stack_b, char ***args)
 {
+	int	i;
+
 	if (stack_a)
 		clear_stack(stack_a);
 	if (stack_b)
 		clear_stack(stack_b);
-	if (args && *args) //Puede que exista args y que sea NULL. Si haces un free(NULL) -> SEGMENTATION FAULT
+	i = 0;
+	if (args && *args)
+	{
+		while ((*args)[i])
+			free((*args)[i++]);
 		free(*args);
+		free(args);
+	}
 	write(1, "Error\n", 6);
 	exit(1);
 }
